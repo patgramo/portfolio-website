e3fa154de47dc76f786094de4b319a86128c4432
Patrick Morrison <patgramo@gmail.com>
2022-09-27 19:55:41 -0700

    Updated JS to change hamburger to x

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+const icon = document.getElementById("my-button")
+
 toggleMenu = () => {
     const smallNav = document.getElementById("small-nav");
         if (smallNav.classList.contains("hide")) {
             smallNav.classList.remove("hide")
             smallNav.classList.add("show")
+            icon.innerHTML= "<i class='fa-solid fa-x'></i>"
            } else if (smallNav.classList.contains("show")) {
             smallNav.classList.remove("show")
             smallNav.classList.add("hide")
+            icon.innerHTML= "<i class='fa-sharp fa-solid fa-bars text-light'></i>"
            }
 }
 clickLink = () => {
@@ -13,5 +17,6 @@ clickLink = () => {
     if (smallNav.classList.contains("show")) {
         smallNav.classList.remove("show")
         smallNav.classList.add("hide")
+        icon.innerHTML= "<i class='fa-sharp fa-solid fa-bars text-light'></i>"
     }
 }
